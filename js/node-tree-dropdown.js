cf047f3f0da8027d4b972631d2665a8963dc681e
byteclean.io <no-reply@byteclean.io>
2019-08-21 03:25:57 +0000

    byteclean.io cleaned these files

--- js/node-tree-dropdown.js
+++ js/node-tree-dropdown.js
@@ -1,62 +1,65 @@
-(function ($) {
+(function($) {
+  var loaded = "";
 
-    var loaded = "";
+  function getPageID() {
+    return $(".js-node-tree-container").data("id");
+  }
 
-    function getPageID() {
-        return $(".js-node-tree-container").data("id");
-    }
+  function makeDropdownTree(rootNode, selected) {
+    $(".js-node-tree-container").treeSelector(
+      rootNode,
+      selected,
+      function(e, values) {
+        $(".selected-nodes").val(values.toString());
+      },
+      {
+        checkWithParent: true, // children checked/unchecked if true
+        titleWithParent: false, // title with 'title1 - title 2' if true
+        notViewClickParentTitle: false, // when item click, only view leaf title if true
+        disabled: false, // disable the plugin
+        emptyOptionPlaceholder: "No Options", // placeholder if empty
+        showToggles: false // Show or hide toggles
+      }
+    );
+  }
 
-    function makeDropdownTree(rootNode, selected) {
-        $(".js-node-tree-container").treeSelector(rootNode, selected, function (e, values) {
-            $(".selected-nodes").val(values.toString());
-        }, {
-            checkWithParent: true,                // children checked/unchecked if true
-            titleWithParent: false,               // title with 'title1 - title 2' if true
-            notViewClickParentTitle: false,       // when item click, only view leaf title if true
-            disabled: false,                      // disable the plugin
-            emptyOptionPlaceholder: "No Options", // placeholder if empty
-            showToggles: false                    // Show or hide toggles
-        });
-    }
+  function requestData(url) {
+    $.ajax(url)
+      .done(function(response) {
+        var data = JSON.parse(response);
+        var treeList = [data.tree][0];
+        var selected = data.selected;
 
-    function requestData(url) {
-        $.ajax(url)
-            .done(function (response) {
-                var data = JSON.parse(response);
-                var treeList = [data.tree][0];
-                var selected = data.selected;
-
-                if (selected[0] === "") {
-                    selected = [];
-                }
-
-                $(".js-node-tree-container").removeClass("lds-dual-ring");
-                makeDropdownTree(treeList, selected);
-                loaded = "dataRequest";
-            })
-            .fail(function (xhr) {
-                console.log("Error: " + xhr.responseText);
-            });
-    }
-
-    function getNodeValues() {
-        $(".js-node-tree-container").addClass("lds-dual-ring");
-        var pageID = getPageID();
-        if (pageID) {
-            var url = "api/nodetree/getTreeData?pageID=" + pageID;
-            requestData(url);
+        if (selected[0] === "") {
+          selected = [];
         }
+
+        $(".js-node-tree-container").removeClass("lds-dual-ring");
+        makeDropdownTree(treeList, selected);
+        loaded = "dataRequest";
+      })
+      .fail(function(xhr) {
+        console.log("Error: " + xhr.responseText);
+      });
+  }
+
+  function getNodeValues() {
+    $(".js-node-tree-container").addClass("lds-dual-ring");
+    var pageID = getPageID();
+    if (pageID) {
+      var url = "api/nodetree/getTreeData?pageID=" + pageID;
+      requestData(url);
     }
+  }
 
-    $(document).ready(function () {
-        getNodeValues();
-    });
+  $(document).ready(function() {
+    getNodeValues();
+  });
 
-    $(document).ajaxComplete(function () {
-        if (loaded !== "dataRequest") {
-            getNodeValues();
-        }
-        loaded = "saveButton";
-    });
-
+  $(document).ajaxComplete(function() {
+    if (loaded !== "dataRequest") {
+      getNodeValues();
+    }
+    loaded = "saveButton";
+  });
 })(jQuery);
